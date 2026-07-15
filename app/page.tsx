1cea32e91405e6980a03ff2a0ece878a16ca0386
luis <l.burgos02@ufromail.cl>
2026-07-15 00:25:35 -0400

    subiendo arquitectura del portafolio

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/sections/Hero/Hero";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Portafolio</h1>
+      <Hero/>
+    </main>
+
+  );
+}
---
+++ sections/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import Container from "@/components/layout/Container";
+
+export default function Hero() {
+  return (
+    <section className="min-h-screen">
+      <Container>
+        Hero
+      </Container>
+    </section>
+  );
+}
